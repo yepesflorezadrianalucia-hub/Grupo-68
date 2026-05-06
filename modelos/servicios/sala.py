9a5374b09b1aa73c05ea930b9d08cc3cb2910f60
yepesflorezadrianalucia-hub <yepesflorezadrianalucia@gmail.com>
2026-05-06 12:32:41 -0500

    Add Sala class with cost calculation method

--- modelos/servicios/sala.py
+++ modelos/servicios/sala.py
@@ -1,1 +1,10 @@
+from .servicio import Servicio
+from excepciones.errores import ServicioError
 
+class Sala(Servicio):
+
+    def calcular_costo(self, duracion):
+        if duracion <= 0:
+            raise ServicioError("Duración inválida", "ERR_DURACION")
+
+        return duracion * self._precio
