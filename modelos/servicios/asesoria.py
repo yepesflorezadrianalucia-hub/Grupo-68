1e5bfadab0a49761155e569291d472b3dd4e9be1
yepesflorezadrianalucia-hub <yepesflorezadrianalucia@gmail.com>
2026-05-06 12:29:20 -0500

    classes de servicios

--- modelos/servicios/asesoria.py
+++ modelos/servicios/asesoria.py
@@ -1,1 +1,22 @@
+from .servicio import Servicio
+from excepciones.errores import ServicioError
 
+class Asesoria(Servicio):
+
+  def __init__(self, id_servicio, nombre, precio, nivel):
+    super().__init__(id_servicio, nombre, precio)
+
+    if nivel not in ["basico", "intermedio", "avanzado"]:
+      raise ServicioError("Nivel Invalido", "ERR_NIVEL")
+
+   self._nivel = nivel 
+
+def calcular_costo(self, duracion):
+  if duracion <= 0:
+    raise ServicioError("Duracion invalida", "ERR_DURACION")
+
+  if self._nivel == "avanzado":
+      return duracion * self._precio * 1.5
+  elif self._nivel == "intermedio":
+      return duracion * self._precio * 1.2
+  return duracion * self._precio
